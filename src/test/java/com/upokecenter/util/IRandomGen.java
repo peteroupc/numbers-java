433734223086a36dd3dea7c779804248c25abd47
Peter O <poccil14@gmail.com>
2016-12-06 12:02:48 -0500

    update Java version

--- src/test/java/com/upokecenter/util/IRandomGen.java
+++ src/test/java/com/upokecenter/util/IRandomGen.java
@@ -6,6 +6,11 @@ package com.upokecenter.util;
  public interface IRandomGen {
     /**
      * Randomly generates a set of bytes.
+     * @param bytes Byte buffer to store the random bytes.
+     * @param offset A zero-based index showing where the desired portion of {@code
+     * bytes} begins.
+     * @param length The length, in bytes, of the desired portion of {@code bytes}
+     * (but not more than {@code bytes} 's length).
      * @return Number of bytes returned.
      */
     int GetBytes(byte[] bytes, int offset, int length);
