d0da8c8b0fbf39186769dcefa0ce6b2af20c8dcd
Peter Occil <poccil14@gmail.com>
2024-12-25 19:46:04 -0500

    update Java version

--- src/main/java/com/upokecenter/numbers/EFloat.java
+++ src/main/java/com/upokecenter/numbers/EFloat.java
@@ -3486,7 +3486,7 @@ Binary16SignifAreaBits;
      * maximum precision allowed if it has more significant digits than the maximum
      * precision. The maximum precision allowed is given in an arithmetic context.
      * This method is designed for preparing operands to a custom arithmetic
-     * operation per the "simplified" arithmetic given in Appendix A
+     * operation in accordance with the "simplified" arithmetic given in Appendix A
      * of the General Decimal Arithmetic Specification.
      * @param ctx An arithmetic context to control the precision, rounding, and
      * exponent range of the result. If {@code HasFlags} of the context is true,
